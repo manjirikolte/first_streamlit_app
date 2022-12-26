14ec49d3b49cfa8cadf82f86d44abbfd016a0236
Manjiree Kolte <47331915+manjirikolte@users.noreply.github.com>
2022-12-26 17:53:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('my parents new healthy diner')
+
+streamlit.header('Breakfast menu')
+streamlit.text('Omega 3 and blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-range Egg')
